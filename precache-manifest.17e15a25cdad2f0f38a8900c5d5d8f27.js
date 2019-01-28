161fe8a9189df3116ec14857a76a703aa375899b
monika patel <monikapatel.it@outlook.com>
2019-01-29 11:29:47 +1300

    Updates

---
+++ precache-manifest.17e15a25cdad2f0f38a8900c5d5d8f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "c8eb6daa9cd2c6ba7c32",
+    "url": "/react-gh-pages/static/js/main.c8eb6daa.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c8eb6daa9cd2c6ba7c32",
+    "url": "/react-gh-pages/static/css/main.17503fba.chunk.css"
+  },
+  {
+    "revision": "d16b8e852cbe26956b031afd7968f9a3",
+    "url": "/react-gh-pages/index.html"
+  }
+];
